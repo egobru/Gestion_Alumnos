08770f2f8742e176511cdf40e1f5baaab079608a
RKS84 <46755517+RKS84@users.noreply.github.com>
2019-05-28 23:41:53 +0200

    Añadidos campos a listarAlumnos()

--- documentos/SPRINTS/SPRINT1/archivos/GUI/datos/consultarAlumnos.js
+++ documentos/SPRINTS/SPRINT1/archivos/GUI/datos/consultarAlumnos.js
@@ -1,20 +1,20 @@
 var alumnos = [
-{'datosMilitares':{'empleoMilitar':{'nombre':'Cbo'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Gizzard',   'apellido2':"",'nif':'11111111B', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'Davison Harley','id':'1111BBB'}], 'foto':'img/student/2.png','nombre':'Jose Luis'}, 'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Cbo1'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Caerar',    'apellido2':"",'nif':'22222222C', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Blanco','nombre':'Focus Ford',    'id':'2222CCC'}], 'foto':'img/student/7.png','nombre':'Julio'},     'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Cbmy'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'egobru',    'apellido2':"",'nif':'33333333D', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Azul',  'nombre':'C5 Citroen',    'id':'33333DDD'}],'foto':'img/student/3.png','nombre':'Quique'},    'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Sgto'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'kzurro',    'apellido2':"",'nif':'44444444F', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Rojo',  'nombre':'A4 Audi',       'id':'4444FFF'}], 'foto':'img/student/5.png','nombre':'Miguel'},    'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Sgto1'},'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'RREEDD12',  'apellido2':"",'nif':'55555555G', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'C Mercedes',    'id':'5555GGG'}], 'foto':'img/student/4.png','nombre':'Julio'},     'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Bg'},   'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'RKS84',     'apellido2':"",'nif':'66666666H', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Blanco','nombre':'Punto Fiat',    'id':'6666HHH'}], 'foto':'img/student/6.png','nombre':'Raul'},      'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Stte'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'RaulGalSan','apellido2':"",'nif':'77777777J', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Azul',  'nombre':'Leon Seat',     'id':'7777JJJ'}], 'foto':'img/student/11.png','nombre':'Raul'},      'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Sbmy'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'CA',        'apellido2':"",'nif':'88888888K', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Rojo',  'nombre':'Fiesta Ford',   'id':'8888KKK'}], 'foto':'img/student/12.png','nombre':'Quique'},    'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Alf'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'aLekos',    'apellido2':"",'nif':'99999999L', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'535 BMW',       'id':'9999LLL'}], 'foto':'img/student/1.png','nombre':'Alejandro'}, 'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Tte'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Ckdt1985',  'apellido2':"",'nif':'111111110M','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Blanco','nombre':'107 Peugeot',   'id':'11110MMM'}],'foto':'img/student/10.png','nombre':'Carlos'},    'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Cap'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Chema003',  'apellido2':"",'nif':'122222221N','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Azul',  'nombre':'156 Alfa Romeo','id':'12221NNN'}],'foto':'img/student/9.png','nombre':'Jose Maria'},'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Cte'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'CarmenDC',  'apellido2':"",'nif':'133333332P','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Rojo',  'nombre':'Cordoba Seat',  'id':'13332PPP'}],'foto':'img/student/8.png','nombre':'Carmen'},    'cursos':[{'nombre':'DIM','id':'null'}]},
-{'datosMilitares':{'empleoMilitar':{'nombre':'Tcol'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'007Patrick','apellido2':"",'nif':'144444443R','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'Vectra Opel',   'id':'14443RRR'}],'foto':'img/student/13.png','nombre':'Patricio'},  'cursos':[{'nombre':'DIM','id':'null'}]}
+{'datosMilitares':{'empleoMilitar':{'nombre':'Cbo'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Gizzard',   'apellido2':"",'nif':'11111111B', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0','1'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'Davison Harley','id':'1111BBB'}], 'foto':'img/student/0.png','nombre':'Jose Luis'}, 'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Cbo1'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Caerar',    'apellido2':"",'nif':'22222222C', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Blanco','nombre':'Focus Ford',    'id':'2222CCC'}], 'foto':'img/student/0.png','nombre':'Julio'},     'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Cbmy'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'egobru',    'apellido2':"",'nif':'33333333D', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Azul',  'nombre':'C5 Citroen',    'id':'33333DDD'}],'foto':'img/student/0.png','nombre':'Quique'},    'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Sgto'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'kzurro',    'apellido2':"",'nif':'44444444F', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Rojo',  'nombre':'A4 Audi',       'id':'4444FFF'}], 'foto':'img/student/0.png','nombre':'Miguel'},    'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Sgto1'},'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'RREEDD12',  'apellido2':"",'nif':'55555555G', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'C Mercedes',    'id':'5555GGG'}], 'foto':'img/student/0.png','nombre':'Julio'},     'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Bg'},   'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'RKS84',     'apellido2':"48SKR",'nif':'66666666H', 'domicilio':{'tipoVia':"Calle",'numeroPortal':"5",'piso':"Esc. izq. 1ºC",'nombre':"de la Academia"},'ciudad':"Hoyo de Manzanares",'telefonos':['0'],'mailsParticular':["rks84@sgal.es"],'vehiculos':[{'color':'Blanco','nombre':'Punto Fiat',    'id':'6666HHH'}], 'foto':'img/student/0.png','nombre':'Raul'},      'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Stte'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'RaulGalSan','apellido2':"",'nif':'77777777J', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Azul',  'nombre':'Leon Seat',     'id':'7777JJJ'}], 'foto':'img/student/0.png','nombre':'Raul'},      'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Sbmy'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'CA',        'apellido2':"",'nif':'88888888K', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Rojo',  'nombre':'Fiesta Ford',   'id':'8888KKK'}], 'foto':'img/student/0.png','nombre':'Quique'},    'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Alf'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'aLekos',    'apellido2':"",'nif':'99999999L', 'domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'535 BMW',       'id':'9999LLL'}], 'foto':'img/student/0.png','nombre':'Alejandro'}, 'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Tte'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Ckdt1985',  'apellido2':"",'nif':'111111110M','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Blanco','nombre':'107 Peugeot',   'id':'11110MMM'}],'foto':'img/student/0.png','nombre':'Carlos'},    'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Cap'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'Chema003',  'apellido2':"",'nif':'122222221N','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Azul',  'nombre':'156 Alfa Romeo','id':'12221NNN'}],'foto':'img/student/0.png','nombre':'Jose Maria'},'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Cte'},  'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'CarmenDC',  'apellido2':"",'nif':'133333332P','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Rojo',  'nombre':'Cordoba Seat',  'id':'13332PPP'}],'foto':'img/student/0.png','nombre':'Carmen'},    'cursos':[{'nombre':'DIM','id':'null'}]},
+{'datosMilitares':{'empleoMilitar':{'nombre':'Tcol'}, 'cuerpo':{'nombre':'CGET'},'mailMilitar':"",'destinoAnterior':""},'datosPersonales':{'apellido1':'007Patrick','apellido2':"",'nif':'144444443R','domicilio':{'tipoVia':"",'numeroPortal':"",'piso':"",'nombre':""},'ciudad':"",'telefonos':['0'],'mailsParticular':[""],'vehiculos':[{'color':'Negro', 'nombre':'Vectra Opel',   'id':'14443RRR'}],'foto':'img/student/0.png','nombre':'Patricio'},  'cursos':[{'nombre':'DIM','id':'null'}]}
     ]
-    file:///E:/CURSO_DE_INFORM%C3%81TICA/2018_Fase_de_presente/Asignaturas/21%20Direccion_Gestion_Proyectos/1_Gestion_Alumnos/IU%20SGAL/HTML%20-%20SPRINT%201/student/1.jpg
-function cargarAlumnos() {
+
+    function cargarAlumnos() {
 
     for (let i = 0; i < alumnos.length; i++) {
         document.querySelector('#orlaAlumnos').innerHTML +=
@@ -43,16 +43,41 @@ function listarAlumnos() {
         var col6 = document.createElement('td');
         var col7 = document.createElement('td');
         var col8 = document.createElement('td');
+        var col9 = document.createElement('td');
+        var col9list = document.createElement('ul');
+        var col9list1 = document.createElement('li');
+        var col9list2 = document.createElement('li');
+        var col10 = document.createElement('td'); 
+        var col10list = document.createElement('ul');
+        var col10list1 = document.createElement('li');
+        var col10list2 = document.createElement('li');     
+        var col10list3 = document.createElement('li');     
+        var col11 = document.createElement('td'); 
+        var col11list = document.createElement('ul');
+        var col11list1 = document.createElement('li');
+        var col11list2 = document.createElement('li');     
+        var col11list3 = document.createElement('li');     
+        var col11list4 = document.createElement('ul');
+        var col11list5 = document.createElement('li');     
         foto.setAttribute('width','70rem');
         foto.src ='' + alumnos[i].datosPersonales["foto"];
-        foto.alt = '' + alumnos[i].datosPersonales["foto"];
-        check.setAttribute('type','checkbox');     
+        check.setAttribute('type','checkbox');
         var infoCol3 = document.createTextNode(alumnos[i].cursos[0]["nombre"]);
         var infoCol4 = document.createTextNode(alumnos[i].datosPersonales["nif"]);
         var infoCol5 = document.createTextNode(alumnos[i].datosPersonales["nombre"]);
-        var infoCol6 = document.createTextNode(alumnos[i].datosPersonales["apellido1"]+ " " + alumnos[i].datosPersonales["apellido2"]);
-        var infoCol7 = document.createTextNode(alumnos[i].datosPersonales["mailsParticular"]);
-        var infoCol8 = document.createTextNode(alumnos[i].datosPersonales.telefonos[0]);
+        var infoCol6 = document.createTextNode(alumnos[i].datosPersonales["apellido1"]);
+        var infoCol7 = document.createTextNode(alumnos[i].datosPersonales["apellido2"]);
+        var infoCol8 = document.createTextNode(alumnos[i].datosPersonales.mailsParticular[0]);
+        var infoCol9a = document.createTextNode(alumnos[i].datosPersonales.telefonos[0]);
+        var infoCol9b = document.createTextNode(alumnos[i].datosPersonales.telefonos[1]);
+        var infoCol10a = document.createTextNode(alumnos[i].datosPersonales.vehiculos[0]["nombre"]);
+        var infoCol10b = document.createTextNode('color: ' + alumnos[i].datosPersonales.vehiculos[0]["color"]);
+        var infoCol10c = document.createTextNode('matrícula: ' + alumnos[i].datosPersonales.vehiculos[0]["id"]);
+        var infoCol11a = document.createTextNode(alumnos[i].datosPersonales.domicilio["tipoVia"]);
+        var infoCol11b = document.createTextNode(alumnos[i].datosPersonales.domicilio["nombre"]);
+        var infoCol11c = document.createTextNode(alumnos[i].datosPersonales.domicilio["numeroPortal"]);
+        var infoCol11d = document.createTextNode(alumnos[i].datosPersonales.domicilio["piso"]);
+        var infoCol11e = document.createTextNode(alumnos[i].datosPersonales["ciudad"]);
         fila.appendChild(col1).appendChild(check);
         fila.appendChild(col2).appendChild(foto);
         fila.appendChild(col3).appendChild(infoCol3);
@@ -61,5 +86,15 @@ function listarAlumnos() {
         fila.appendChild(col6).appendChild(infoCol6);
         fila.appendChild(col7).appendChild(infoCol7);
         fila.appendChild(col8).appendChild(infoCol8);
+        fila.appendChild(col9).appendChild(col9list).appendChild(col9list1).appendChild(infoCol9a);
+        col9list.appendChild(col9list2).appendChild(infoCol9b);
+        fila.appendChild(col10).appendChild(col10list).appendChild(col10list1).appendChild(infoCol10a);  
+        col10list.appendChild(col10list2).appendChild(infoCol10b);
+        col10list.appendChild(col10list3).appendChild(infoCol10c);
+        fila.appendChild(col11).appendChild(col11list).appendChild(col11list1).appendChild(infoCol11a);  
+        col11list.appendChild(col11list2).appendChild(infoCol11b);
+        col11list.appendChild(col11list3).appendChild(infoCol11c);
+        col11list.appendChild(col11list4).appendChild(infoCol11d);
+        col11list.appendChild(col11list5).appendChild(infoCol11e);
     }
 }
